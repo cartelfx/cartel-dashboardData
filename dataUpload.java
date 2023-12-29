265dcf14e40cfaad435f5c5a3de423d4d4968ab9
cartel <119452317+cartelfx@users.noreply.github.com>
2023-12-29 16:26:34 +0400

    Create dataUpload.java

---
+++ dataUpload.java
@@ -0,0 +1,34 @@
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.HttpClients;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class DashboardDataUploader {
+    public static void main(String[] args) {
+        HttpClient httpClient = HttpClients.createDefault();
+        HttpPost httpPost = new HttpPost("URL_OF_YOUR_DASHBOARD_API");
+
+        try {
+            String jsonData = "{ \"username\": \"example_user\", \"data\": \"your_data\" }";
+
+            StringEntity entity = new StringEntity(jsonData);
+            httpPost.setEntity(entity);
+            httpPost.setHeader("Accept", "application/json");
+            httpPost.setHeader("Content-type", "application/json");
+            HttpResponse response = httpClient.execute(httpPost);
+            BufferedReader reader = new BufferedReader(new InputStreamReader(response.getEntity().getContent()));
+            String line;
+            StringBuilder result = new StringBuilder();
+            while ((line = reader.readLine()) != null) {
+                result.append(line);
+            }
+          System.out.println("HTTP Response: " + result.toString());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
